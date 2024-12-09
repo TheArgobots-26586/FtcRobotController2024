da8cba82fa0f5459a3b57ba89658cf2ecdd99d70
Avyay <avyay.vivek@gmail.com>
2024-12-08 16:46:25 -0800

    Added Servo Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,45 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/* Code to test servos. */
+
+@TeleOp(name="Basic: Servo Linear OpMode", group="Linear OpMode")
+public class ServoTest extends LinearOpMode {
+    private Servo wrist_servo = null;
+    private CRServo intake_servo = null;
+
+    @Override
+    public void runOpMode() {
+        wrist_servo = hardwareMap.get(Servo.class,"wrist");
+        intake_servo = hardwareMap.get(CRServo.class,"intake");
+        wrist_servo.setDirection(Servo.Direction.REVERSE);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("current position:",wrist_servo.getPosition());
+            if (gamepad1.cross) {
+                wrist_servo.setPosition(1.0);
+            }
+            else if (gamepad1.circle) {
+                wrist_servo.setPosition(0.0);
+
+            }
+            else if (gamepad1.triangle) {
+                wrist_servo.setPosition(0.5);
+            }
+            if (gamepad1.dpad_up) {
+                intake_servo.setPower(1.0);
+            }
+            else if (gamepad1.dpad_down) {
+                intake_servo.setPower(0.0);
+            }
+            telemetry.update();
+        }
+    }
+}
+
